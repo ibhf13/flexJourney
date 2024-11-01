ee9cc3ecd7ddbbe710ac837fd93367f3e6004258
ibhf13 <ibhf13@gmail.com>
2024-11-01 10:49:15 +0100

    refactoring

--- src/features/workout/components/common/MediaWithSkeleton.tsx
+++ src/features/workout/components/common/MediaWithSkeleton.tsx
@@ -1,32 +1,33 @@
 import { Box, CardMedia, Skeleton, SxProps, Theme } from '@mui/material'
+import { useState } from 'react'
 
 interface MediaWithSkeletonProps {
     height: number
     imageUrl: string
-    imageLoaded: boolean
-    onLoad: () => void
     mediaStyles?: SxProps<Theme>
 }
 
 export const MediaWithSkeleton = ({
     height,
     imageUrl,
-    imageLoaded,
-    onLoad,
     mediaStyles
-}: MediaWithSkeletonProps) => (
-    <Box sx={{ position: 'relative' }}>
-        {!imageLoaded && <Skeleton variant="rectangular" height={height} />}
-        <CardMedia
-            component="img"
-            height={height}
-            image={imageUrl}
-            alt=""
-            sx={{
-                ...mediaStyles,
-                display: imageLoaded ? 'block' : 'none'
-            }}
-            onLoad={onLoad}
-        />
-    </Box>
-)
+}: MediaWithSkeletonProps) => {
+    const [imageLoaded, setImageLoaded] = useState(false)
+
+    return (
+        <Box sx={{ position: 'relative' }}>
+            {!imageLoaded && <Skeleton variant="rectangular" height={height} />}
+            <CardMedia
+                component="img"
+                height={height}
+                image={imageUrl}
+                alt=""
+                sx={{
+                    ...mediaStyles,
+                    display: imageLoaded ? 'block' : 'none'
+                }}
+                onLoad={() => setImageLoaded(true)}
+            />
+        </Box>
+    )
+}
