48200397a67807a23b657d2de76a3532bb654bc7
ibhf13 <ibhf13@gmail.com>
2024-12-03 20:10:50 +0100

    delete exercises

--- src/features/exercises/components/ExerciseCard.tsx
+++ src/features/exercises/components/ExerciseCard.tsx
@@ -1,12 +1,16 @@
 import { BaseCard, BaseCardContent } from '@/components/common/Cards'
 import { DifficultyLevel } from '@/features/workout/types/WorkoutTypes'
+import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
-import { Chip, IconButton } from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { Chip, IconButton, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material'
+import { useState } from 'react'
 import { Exercise } from '../types/ExerciseTypes'
 
 interface ExerciseCardProps {
     exercise: Exercise
     onEdit?: (exercise: Exercise) => void
+    onDelete?: (exercise: Exercise) => void
     onView?: (exercise: Exercise) => void
     isAdmin?: boolean
 }
@@ -14,18 +18,38 @@ interface ExerciseCardProps {
 export const ExerciseCard = ({
     exercise,
     onEdit,
+    onDelete,
     onView,
     isAdmin = false
 }: ExerciseCardProps) => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+
     const handleCardClick = () => {
         onView?.(exercise)
     }
 
-    const handleEditClick = (e: React.MouseEvent) => {
-        e.stopPropagation()
+    const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        event.stopPropagation()
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleMenuClose = (event: React.MouseEvent) => {
+        event.stopPropagation()
+        setAnchorEl(null)
+    }
+
+    const handleEditClick = (event: React.MouseEvent) => {
+        event.stopPropagation()
+        setAnchorEl(null)
         onEdit?.(exercise)
     }
 
+    const handleDeleteClick = (event: React.MouseEvent) => {
+        event.stopPropagation()
+        setAnchorEl(null)
+        onDelete?.(exercise)
+    }
 
     return (
         <BaseCard
@@ -34,23 +58,44 @@ export const ExerciseCard = ({
             imageHeight={200}
             onClick={handleCardClick}
             actionButton={isAdmin && (
-                <IconButton
-                    onClick={handleEditClick}
-                    sx={{
-                        position: 'absolute',
-                        top: 8,
-                        right: 8,
-                        width: 24,
-                        height: 24,
-                        bgcolor: 'background.paper',
-                        '&:hover': {
-                            bgcolor: 'action.hover',
-                        },
-                        zIndex: 1
-                    }}
-                >
-                    <EditIcon fontSize="small" />
-                </IconButton>
+                <>
+                    <IconButton
+                        onClick={handleMenuOpen}
+                        sx={{
+                            position: 'absolute',
+                            top: 8,
+                            right: 8,
+                            width: 24,
+                            height: 24,
+                            bgcolor: 'background.paper',
+                            '&:hover': {
+                                bgcolor: 'action.hover',
+                            },
+                            zIndex: 1
+                        }}
+                    >
+                        <MoreVertIcon fontSize="small" />
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleMenuClose}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <MenuItem onClick={handleEditClick}>
+                            <ListItemIcon>
+                                <EditIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText>Edit</ListItemText>
+                        </MenuItem>
+                        <MenuItem onClick={handleDeleteClick}>
+                            <ListItemIcon>
+                                <DeleteIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText>Delete</ListItemText>
+                        </MenuItem>
+                    </Menu>
+                </>
             )}
             sx={{
                 position: 'relative',
