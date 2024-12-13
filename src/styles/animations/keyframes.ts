4addde164e7309adfe5193031c581f1371dc8d50
ibhf13 <ibhf13@gmail.com>
2024-12-13 21:47:47 +0100

    refactor congrats

---
+++ src/styles/animations/keyframes.ts
@@ -0,0 +1,7 @@
+import { keyframes } from '@emotion/react'
+
+export const bounceAnimation = keyframes`
+  0%, 20%, 50%, 80%, 100% { transform: translateY(0); }
+  40% { transform: translateY(-20px); }
+  60% { transform: translateY(-10px); }
+` 
